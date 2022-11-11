1346f563993ba7dfe6b234101c1aff91e726b454
Paolo-Castagno <117978458+Paolo-Castagno@users.noreply.github.com>
2022-11-11 11:28:20 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,13 @@ using namespace std;
 
 int main(){
 //Qui vanno definite le variabili
-
+float a,b,c;
 cin >> a >> b >> c;
 
 //Qui va scritto il codice per eseguire i calcoli richiesti
-
+cout << a * b / 2 <<endl; //Area triangolo
+cout << a * a <<endl; //Area quadrato
+cout << a * b <<endl; // Area rettangolo
+cout << (a+b) * c /2 <<endl; //Area trapezio
 
 }
